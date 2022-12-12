14f8356863c82bef033ab4aba6a924400b7f08f8
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-12 06:48:12 -0800

    If, else

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Print 2 different number
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('\n');
+	return (0);
+}
