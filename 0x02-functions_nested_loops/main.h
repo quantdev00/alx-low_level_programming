fb3de95debf5f0c7811fd9e5816870707b7e5868
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-13 07:07:10 -0800

    all

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,12 @@
  * Putchar prototype
  */
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
