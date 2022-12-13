4fc0b208c2a162a1b5ecf3f7ddf14d65d44ecfcd
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-13 07:03:31 -0800

    all

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	
 	void print_alphabet(void)
-{
+	{
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-}
+	}
 	print_alphabet();
 	return (0);
 }
