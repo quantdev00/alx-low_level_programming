1a3bec7e7234588057e0487f2d563a26c1dc3bd0
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-14 07:06:59 -0800

    new

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,8 +1,9 @@
-#include "main.h"
 #include "1-alphabet.c"
+#include "_putchar.c"
+#include "2-print_alphabet_x10.c"
 
 int main(void)
 {
-	print_alphabet();
+	print_alphabet_x10();
 	return (0);
 }
