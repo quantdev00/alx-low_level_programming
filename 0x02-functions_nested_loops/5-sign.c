97a18686e524b4a5495c79016d6265f3ccd5aa87
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-14 12:17:13 -0800

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,23 +12,16 @@ int print_sign(int  n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('\n');
 		return (1);
 	} else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('\n');
 		return (0);
 	} else
 	{
 		_putchar('-');
-		_putchar('1');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('\n');
 		return (1);
 	}
