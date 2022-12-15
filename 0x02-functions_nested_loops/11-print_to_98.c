b393ef8b512ce509eb7d70dc83754f766be3806d
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-15 06:35:15 -0800

    all

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int  n)
 	{
 		for (i = n; i <= 98; i++)
 		{	printf("%d", i);
-			if (n == 98)
+			if (i == 98)
 				printf("\n");
 			else
 				printf(", ");
@@ -25,7 +25,7 @@ void print_to_98(int  n)
 	{
 		for (i = n; i >= 98; i--)
 		{	printf("%d", i);
-			if (n != 98)
+			if (i != 98)
 				printf(", ");
 		}
 	}
