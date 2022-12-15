632d9dfa8645d948f67f34a02a88d07ede4b4264
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-15 03:42:19 -0800

    all

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,7 @@
  */
 int print_last_digit(int  n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
 	int last_digit = n % 10;
 
@@ -20,7 +20,7 @@ int print_last_digit(int  n)
 	{
 		int last_digit_negative = ((n % 10) * -1);
 
-		_putchar(last_digit_negative);
+		_putchar(last_digit_negative + '0');
 		return (last_digit_negative);
 	}
 }
