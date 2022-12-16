de6ea72c00d2fc5947a403300eeb35c5b3719b6a
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-16 13:42:51 -0800

    all

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
+	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0) && (day <= 31 && day >= 1) && (month <= 12 && month >= 1))
 	{
 		if (month >= 2 && day >= 60)
 		{
