6cd472c593b050f7946fc9ef81a3ae5e9e50f75c
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-19 09:42:34 -0800

    all

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints a to z'
+ *
+ * Return: 0;
+ */
+void print_numbers(void)
+{
+	char i;
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int, int);
+void print_numbers(void);
 #endif /* MAIN_H */
