a0298b0d29d0a61a9cdf49c8144c2e93084ef5ba
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-20 08:04:49 -0800

    all

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset tthe number to 98
+ * @*n: the number to use
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int *p = &n;
+	*p = 98;
+}
