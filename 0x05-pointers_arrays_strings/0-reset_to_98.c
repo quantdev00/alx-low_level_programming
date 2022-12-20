6dd28a375f49d04b9a3027504da7b499856a5a4e
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-20 08:08:25 -0800

    all

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
 
 /**
  * reset_to_98 - reset tthe number to 98
- * @*n: the number to use
+ * @n: the number to use
  * Return: 0
  */
 void reset_to_98(int *n)
 {
-	int *p = &n;
+	int **p = &n;
 	*p = 98;
 }
