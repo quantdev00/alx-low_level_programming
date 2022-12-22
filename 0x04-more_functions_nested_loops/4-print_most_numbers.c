b766956c8dd7760dafca66b9165e56cea2d5cc32
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-22 02:56:26 -0800

    all

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * print_most_numbers - prints a to z'
  *
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int, int);
 void print_numbers(void);
-void print_more_numbers(void);
+void print_most_numbers(void);
 
 #endif
