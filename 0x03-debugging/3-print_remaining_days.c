2c7cf950df80fcfae245b93459821d33d1650d72
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-22 01:41:27 -0800

    all

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,15 +12,17 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-		if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
+		if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0))
 		{
 			if (month >= 2 && day >= 60)
 			{
 				day++;
 			}
-
+			if ((12 >= month && 2 <= month) && day <= 366)
+			{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
+			}
 		}
 		else
 		{
@@ -30,8 +32,11 @@ void print_remaining_days(int month, int day, int year)
 			}
 			else
 			{
+				if ((12 >= month && 2 <= month) && day <=365)
+				{
 				printf("Day of the year: %d\n", day);
 				printf("Remaining days: %d\n", 365 - day);
+				}
 			}
 		}
 }
