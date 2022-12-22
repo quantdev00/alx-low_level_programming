8569598bd7bffb7e15b2569c2c91c86243b411c2
Daniel Kiyiki <danielngolu154@gmail.com>
2022-12-22 03:46:06 -0800

    al

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,15 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	long int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
-			{
 				_putchar('1');
-				_putchar(j % 10 + '0');
-			}
-			_putchar(j + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
