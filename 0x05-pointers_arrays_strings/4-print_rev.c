c9ee02af972e9e64f09511814f4522e31e8588af
Daniel Kiyiki <danielngolu154@gmail.com>
2023-01-01 12:42:20 -0800

    alx task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
 
 /**
  * print_rev - reset tthe number to 98
- * @str: the number to use
+ * @s: the number to use
  * Return: 0
  */
 void print_rev(char *s)
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int i;
 	int length = strlen(s);
 
-	for (i = length-1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		fprintf(stdout, "%c", s[i]);
 	}
