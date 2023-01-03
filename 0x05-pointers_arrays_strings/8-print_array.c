8a43b41090a01c8cbf06b9a578abc94cf94f28ef
Daniel Kiyiki <danielngolu154@gmail.com>
2023-01-03 11:19:03 -0800

    alx

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,9 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf("%d, ", a[i]);
+		if (i != n)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 	printf("\n");
 }
