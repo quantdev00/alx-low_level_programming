a8102b49952003a4a6c93e9cd7f77418fc057927
Daniel Kiyiki <danielngolu154@gmail.com>
2023-01-03 11:45:40 -0800

    new

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign
+ * @dest: to be returned
+ * @src: string to copy
+ *
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
