0279adb1378ab6575e5490bdf49911b6cebb7b2d
Daniel Kiyiki <danielngolu154@gmail.com>
2023-01-04 07:32:49 -0800

    alx task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,33 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	
+	char tmp[100];
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		tmp[j] = dest[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+	tmp[j] = ' ';
+	j = j + 1;
+	while (src[i] != '\0')
+	{
+		tmp[j] = src[i];
+		i++;
+		j++;
+	}
+	tmp[j] = '\0';
+	i = 0;
+
+	while (tmp[i] != '\0')
+	{
+		dest[i] = tmp[i];
+		i++;
+	}
+	return (dest);
 }
