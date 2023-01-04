cde6a870673f318efbaa6a2d0bb1c3751d74c810
Daniel Kiyiki <danielngolu154@gmail.com>
2023-01-04 13:25:49 -0800

    new

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,21 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int ln1 = strlen(s1);
+	int ln2 = strlen(s2);
+	char last_char1 = s1[ln1 - 1];
+	char last_char2 = s2[ln2 - 1];
+
+	if (ln1 < ln2)
+		last_char1 = '0';
+	else if (ln2 < ln1)
+		last_char2 = '0';
+	int result = last_char1 - last_char2;
+
 	if (s1 < s2)
-		return (-15);
+		return (result);
 	else if (s1 > s2)
-		return (15);
+		return (result);
 	else if (s1 == s2)
-		return (0);
-	else
 		return (0);
 }
