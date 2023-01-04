f15154848a6804c5b81b39ca9880b3970b8f0031
Daniel Kiyiki <danielngolu154@gmail.com>
2023-01-04 13:32:30 -0800

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int ln1 = strlen(s1);
-	int ln2 = strlen(s2);
-	char last_char1 = s1[ln1 - 1];
-	char last_char2 = s2[ln2 - 1];
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-	if (ln1 < ln2)
-		last_char1 = '0';
-	else if (ln2 < ln1)
-		last_char2 = '0';
-	int result = last_char1 - last_char2;
-
-	if (s1 < s2)
-		return (result);
-	else if (s1 > s2)
-		return (result);
-	else if (s1 == s2)
+	if (*s1 == *s2)
 		return (0);
+	else
+		return (*s1 - *s2);
 }
