8665a7095b445f9907884294e0bab912e4b07cbd
quantdev00 <danielngolu154@gmail.com>
2023-01-07 08:17:00 -0800

    alx task;

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * *string_toupper - reversing the received array
+ * @*: the char
+ *
+ * Return: 0;
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+	
+	while (*[i] != '\0')
+	{
+		toupper(*[i]);
+		i++;
+	}
+}
