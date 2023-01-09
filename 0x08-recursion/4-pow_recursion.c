a8ca2e3010867470cfb69cecc838414d404a3a0a
quantdev00 <danielngolu154@gmail.com>
2023-01-09 13:11:48 -0800

    new com

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,14 +6,12 @@
  * @y: num tw0
  * Return: an int;
  */
-int _pow_recursion(int x, int y){
+int _pow_recursion(int x, int y)
+{
 	if (y < 0)
 		return (-1);
 	if (y != 0)
-	{
-		return (x * _pow_recursion(x, y -1));
-	}
-	else {
+		return (x * _pow_recursion(x, y - 1));
+	else
 		return (1);
-	}
 }
